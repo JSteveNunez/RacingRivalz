4f57baa7c68354e1cbfaa4f262d9b6c5cb1e0f99
miken <miken@partyrentalltd.com>
2014-08-23 17:53:53 -0400

    repacking

---
+++ mobile/src/main/java/com/artoos/racing/models/Race.java
@@ -0,0 +1,17 @@
+package com.artoos.racing.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Nakhimovich on 8/23/14.
+ */
+public class Race
+{
+    public String name;
+    public boolean isDistance;
+    public double raceValue;
+    public Map<String, Racer> racers = new HashMap<String, Racer>();
+
+
+}
---
+++ mobile/src/main/java/com/artoos/racing/models/Racer.java
@@ -0,0 +1,12 @@
+package com.artoos.racing.models;
+
+/**
+ * Created by Nakhimovich on 8/23/14.
+ */
+public class Racer
+{
+   public String name;
+   public Double speed;
+   public Double distance;
+   public Integer steps;
+}
