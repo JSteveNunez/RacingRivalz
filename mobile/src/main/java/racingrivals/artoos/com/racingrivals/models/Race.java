7c234a772eaf84ea10e4134de48571824c50b5d3
miken <miken@partyrentalltd.com>
2014-08-23 15:39:49 -0400

    firebase sample

---
+++ mobile/src/main/java/racingrivals/artoos/com/racingrivals/models/Race.java
@@ -0,0 +1,14 @@
+package racingrivals.artoos.com.racingrivals.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Nakhimovich on 8/23/14.
+ */
+public class Race
+{
+    public String name;
+    public Map<String, Racer> racers = new HashMap<String, Racer>();
+
+}
---
+++ mobile/src/main/java/racingrivals/artoos/com/racingrivals/models/Racer.java
@@ -0,0 +1,12 @@
+package racingrivals.artoos.com.racingrivals.models;
+
+/**
+ * Created by Nakhimovich on 8/23/14.
+ */
+public class Racer
+{
+   public String name;
+   public Double speed;
+   public Double distance;
+   public Integer steps;
+}
